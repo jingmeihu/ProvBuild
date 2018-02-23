89ce2ae7355562d08aa0b54bb48565d649474a9f
CrystalMei <jingmei_hu@g.harvard.edu>
2018-02-22 22:29:10 -0500

    comment unnecessary terminal echo

--- capture/noworkflow/make.sh
+++ capture/noworkflow/make.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
-echo "I have several commands: r-run, uf-update function, uv-update variable, d-runupdate, m-merge"
+# echo "I have several commands: r-run, uf-update function, uv-update variable, d-runupdate, m-merge"
 case "$1" in
 	r)
-		echo "Remove the .noworkflow folder ..."
+		# echo "Remove the .noworkflow folder ..."
 		rm -r .noworkflow
+		rm ProvScript.py
 		echo "The test file name is $2 ..."
 		python __init__.py run "$2"
 		;;
 	uf)
-		echo "The trial id is 1 ..."
+		# echo "The trial id is 1 ..."
 		echo "The function name is $2 ..."
 		python __init__.py update -t 1 -fn "$2" --debug 0
 		;;
 	uv)
-		echo "The trial id is 1 ..."
+		# echo "The trial id is 1 ..."
 		echo "The variable name is $2 ..."
 		python __init__.py update -t 1 -vn "$2" --debug 0
 		;;
@@ -23,7 +24,7 @@ case "$1" in
 		python ProvScript.py
 		;;
 	m)
-		echo "The trial id is 1 ..."
+		# echo "The trial id is 1 ..."
 		echo "Generate new file ..."
 		python __init__.py merge -t 1
 		;;
